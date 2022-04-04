04273bba51878444edcf71badd1120c88d119179
Cédric Creusot <cedric.creusot@gmail.com>
2022-04-04 13:36:02 +0200

    add parameterized test

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,34 @@ fn main() {
 }
 
 enum Destination {
-    B
+    B,
 }
 
-fn compute_time_to_deliver(destination: Vec<Destination>) -> i32 {
-    5
+fn compute_time_to_deliver(destination: Vec<Destination>) -> usize {
+    if destination.is_empty() {
+        0
+    } else {
+        let size = destination.len();
+        ((size - 1) / 2) * 10 + 5
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn should_simplest_case() {
+    use super::Destination::{self, *};
+    use rstest::rstest;
 
-        let hours_to_deliver: i32 = compute_time_to_deliver(vec![Destination::B]);
-        assert_eq!(5, hours_to_deliver);
-    }
-
-    #[test]
-    fn should_compute_time_with_2_destinations() {
-
-        let hours_to_deliver: i32 = compute_time_to_deliver(vec![Destination::B, Destination::B]);
-        assert_eq!(5, hours_to_deliver);
+    #[rstest]
+    #[case(vec![], 0)]
+    #[case(vec![B], 5)]
+    #[case(vec![B, B], 5)]
+    #[case(vec![B, B, B], 15)]
+    #[case(vec![B, B, B, B], 15)]
+    #[case(vec![B, B, B, B, B], 25)]
+    #[case(vec![B, B, B, B, B, B, B], 35)]
+    fn should_compute_time_with_2_trucks(#[case] dest: Vec<Destination>, #[case] expect: usize) {
+        let hours_to_deliver = compute_time_to_deliver(dest);
+        assert_eq!(expect, hours_to_deliver);
     }
 }
